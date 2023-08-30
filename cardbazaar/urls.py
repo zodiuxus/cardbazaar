26829f955cdfd6694a5567507b8f7823923c96ca
zodiuxus <privatepropeller@protonmail.com>
2023-08-30 16:29:28 +0200

    Update urls.py

--- cardbazaar/urls.py
+++ cardbazaar/urls.py
@@ -19,7 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("mainpage.urls")),
-    path("polls/", include("polls.urls")),
     path("account/", include("account.urls")),
     path('admin/', admin.site.urls),
 ]
